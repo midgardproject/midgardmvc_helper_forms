d1344a3677972ef2271d45f5be12f76581ac484d
Henri Bergius <henri.bergius@iki.fi>
2010-12-01 14:41:15 +0200

    Hidden form widget support

---
+++ widget/hidden.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package midgardmvc_helper_forms
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+class midgardmvc_helper_forms_widget_hidden extends midgardmvc_helper_forms_widget
+{
+
+    public function __toString()
+    {    
+        return $this->add_label("<input type='hidden' name='{$this->field->get_name()}' value='{$this->field->get_value()}' {$this->get_attributes()}/>");
+    }
+
+}
+?>
