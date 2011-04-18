ca76c5c48a9462a6d8db9d97f261e696ae7b7236
Henri Bergius <henri.bergius@iki.fi>
2011-04-18 16:15:32 +0300

    Ensure float widgets work on Chrome

--- widget/number.php
+++ widget/number.php
@@ -14,8 +14,12 @@ class midgardmvc_helper_forms_widget_number extends midgardmvc_helper_forms_widg
 
     public function __toString()
     {
+        $step = '';
+        if ($this->field instanceof midgardmvc_helper_forms_field_float) {
+            $step = ' step="any"';
+        }
         $value = str_replace(',', '.', $this->field->get_value());
-        return $this->add_label("<input type='number' name='{$this->field->get_name()}' value='{$value}' {$this->get_attributes()}/>");
+        return $this->add_label("<input type='number'{$step} name='{$this->field->get_name()}' value='{$value}' {$this->get_attributes()}/>");
     }
 
 }
